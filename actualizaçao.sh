9b6332d9832f8e15002960023408bcfead50245c
meepmaster360 <67827146+meepmaster360@users.noreply.github.com>
2020-11-07 15:43:36 +0000

    Add files via upload

---
+++ actualizaçao.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Script Automatizado para atualização e limpeza.
+
+#INICIO
+sudo apt-get update 
+apt-get upgrade 
+echo "Update e Upgrade concluído";sleep 1
+echo "Iniciando a Limpeza do Sistema";sleep 1
+apt-get autoremove
+apt-get autoclean
+apt-get clean
+echo "Limpeza concluída";sleep 1
+echo "Tenha um bom dia" 
+#FIM
