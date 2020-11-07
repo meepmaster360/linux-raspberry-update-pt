f415c609302bef7b89ee7358446b72b7eaae6bc7
meepmaster360 <67827146+meepmaster360@users.noreply.github.com>
2020-11-07 22:04:58 +0000

    Update actualizaçao.sh

--- actualizaçao.sh
+++ actualizaçao.sh
@@ -4,6 +4,7 @@
 #INICIO
 sudo apt-get update 
 apt-get upgrade 
+sudo apt-get dist-upgrade
 echo "Update e Upgrade concluído";sleep 1
 echo "Iniciando a Limpeza do Sistema";sleep 1
 apt-get autoremove
