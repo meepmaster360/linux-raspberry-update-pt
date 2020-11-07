d9100b77f34f6d771d746d2854b08ab14ab24bd1
meepmaster360 <67827146+meepmaster360@users.noreply.github.com>
2020-11-07 22:30:23 +0000

    Update actualizaçao.sh

--- actualizaçao.sh
+++ actualizaçao.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Script Automatizado para atualização e limpeza.
+#Script Automatizado para actualização e limpeza do sistema.
 
 #INICIO
 echo "Actualização de Sistema Linux.";sleep 1
@@ -8,21 +8,21 @@ clear
 sudo apt-get update 
 apt-get upgrade 
 sudo apt-get dist-upgrade
-echo "Update e Upgrade concluído";sleep 1
+echo "Update e Upgrade concluído.";sleep 1
 # Opção de RPI-Upgrade, com um "if-then-read"
 echo "Deseja instalar RPI-Upgrade? (s/n)"
 read rpiup
 if [ "$rpiup" == s ]
 then
-sudo rpi-upgrade
+sudo rpi-update
 elif [ "$rpiup == n ]
 then
-echo "Maravilha, vamos continuar"
+echo "Maravilha, vamos continuar."
 fi
-echo "Iniciando a Limpeza do Sistema";sleep 1
+echo "Iniciando a Limpeza do Sistema.";sleep 1
 apt-get autoremove
 apt-get autoclean
 apt-get clean
-echo "Limpeza concluída";sleep 1
-echo "Tenha um bom dia" 
-#FIM
+echo "Limpeza concluída.";sleep 1
+echo "Tenha um bom dia." 
+#Espero que tenham gostado.
