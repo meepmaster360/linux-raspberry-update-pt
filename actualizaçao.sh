3f119a4d334e14bf1c517173ad6e4f5e8e9af2b8
meepmaster360 <67827146+meepmaster360@users.noreply.github.com>
2020-11-07 22:38:39 +0000

    Update actualizaçao.sh

--- actualizaçao.sh
+++ actualizaçao.sh
@@ -2,11 +2,11 @@
 #Script Automatizado para actualização e limpeza do sistema.
 
 #INICIO
-echo "Actualização de Sistema Linux.";sleep 1
+echo "Actualização de Sistema Linux.";sleep 3
 clear
 # Update e Upgrade do sistema
 sudo apt-get update 
-apt-get upgrade 
+sudo apt-get upgrade 
 sudo apt-get dist-upgrade
 echo "Update e Upgrade concluído.";sleep 1
 # Opção de RPI-Upgrade, com um "if-then-read"
@@ -20,9 +20,9 @@ then
 echo "Maravilha, vamos continuar."
 fi
 echo "Iniciando a Limpeza do Sistema.";sleep 1
-apt-get autoremove
-apt-get autoclean
-apt-get clean
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo apt-get clean
 echo "Limpeza concluída.";sleep 1
 echo "Tenha um bom dia." 
 #Espero que tenham gostado.
